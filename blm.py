7af74b0c682ed735f24c4e6a552d4724ff892bcb
msaavedra123 <43019009+msaavedra123@users.noreply.github.com>
2018-11-15 14:07:43 -0500

    Update blm.py

--- blm.py
+++ blm.py
@@ -1,34 +1,60 @@
 '''
-The variables s and t represent two of five questions that the user will be asked.'''
+Was unable to figure out how to construct a class, so have five separate functions instead.
+The variables p, s, t, h, o and c represent each of the six questions the user will be answering. '''
 
-s = raw_input("1. In 2017, how many soldiers died in action around the globe? ")
-t = raw_input("2. In 2017, how many American died in the United States because of terrorism? ")
+'''for def planes(), p is the user answer for question 1,  if the answer is correct it will print "correct."
+If answer is a letter instead of a number or incorrect, will print incorrect with error message and reprint the question so the user.
+For each answer it will be checking for a string and not an int because of unexpected errors.'''
 
-'''Def main is to combine all the functions that will be taking place, in the overall program,
-this was done because it allows me to see the output of each individual input, or answer to
-the questions. Currently, the only issue I have is making Incorrect or correct print after
-each question instead of at the end of all
-the questions.'''
+def planes():
+	print("Please enter whole numbers only to answer each question.")
+	p = raw_input("1. How many citizens were killed in plane crashes in 2017? ")
+	if p == "13":
+		print("Correct")
+	else:
+		print("Incorrect, make sure to enter a whole number!")
+def soldiers():
+	s = raw_input("2. In 2017, how many soldiers died in action around the globe? ")
+	if s == "21":
+		print("Correct")
+	else:
+		print("Incorrect, make sure to enter a whole number!")
+		#s = raw_input("2. In 2017, how many soldiers died in action around the globe? ")
+def terror():
+	t = raw_input("3. In 2017, how many American died in the United States because of terrorism? ")
+	if t == "4":
+		print("Correct")
+	else:
+		print("Incorrect, make sure to enter a whole number!")
+		#t = raw_input("3. In 2017, how many American died in the United States because of terrorism? ")
+def shooter():
+	h = raw_input("4. How many people were killed by mass shooters in 2017? ")
+	if h == "428":
+		print("Correct")
+	else:
+		print("Incorrect, make sure to enter a whole number!")
+		#h = raw_input("4. How many people were killed by mass shooters in 2017? ")
+def chicago():
+	c = raw_input("5. How many deaths were caused by Chicago's 'top gangs' in 2017? ")
+	if c == "675":
+		print("Correct")
+	else:
+		print("Incorrect, make sure to enter a whole number!")
+		#c = raw_input("5. How many deaths were caused by Chicago's 'top gangs' in 2017? ")
+def police():
+	o = raw_input("6. In 2017, how many people were killed by police? ")
+	if o == "1147":
+		print("Correct")
+	else:
+		print("Incorrect, make sure to enter a whole number!")
+		#o = raw_input("6. In 2017, how many people were killed by police? ")
 
-def main():
-	'''for def soldiers(s), s is the user answer for question 1, it is taken as the argument,
-	if the answer is correct it will print "correct" if it is incorrect "incorrect" will be printed,
-	no particular value will be returned'''
-	def soldiers(s):
-		s = int(s)
-		if s == 21:
-			print("Correct")
-		elif s != 21:
-			print("Incorrect")
-	'''def terror(t), works the same way as def soldiers(s), where t is the user input for question
-	2 no value is to be returned'''
-	def terror(t):
-		t = int(t)
-		if terror == 4:
-			print("Correct")
-		elif t != 4:
-			print("Incorrect")
-
-	soldiers(s)
-	terror(t)
-main()
+planes()
+soldiers()
+terror()
+shooter()
+chicago()
+police()
+''' Plan to print names of various police brutality victims names were located in a database from https://policeviolencereport.org/
+Main article located: https://www.theroot.com/heres-how-many-people-police-killed-in-2017-1821706614 , it summarizes all data but includes links to the main
+sources'''
